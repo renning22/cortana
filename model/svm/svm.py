cd760c7fd16ee12f2b84937797d830f8d23040a1
Ning Ren <renning22@gmail.com>
2014-02-15 19:29:56 +0800

    svm dump model optional

--- model/svm/svm.py
+++ model/svm/svm.py
@@ -15,13 +15,13 @@ import codecs
 import argparse
 from scipy.sparse import csr_matrix
 
-def linear_train(modelfile,trainmatfile,vs='1vsR',C=1,regularize='l2',gridsearch=False):
+def linear_train(trainfile,testfile,vs='1vsR',C=1,regularize='l2', dumpmodel=False):
     ""
 
-    log._logger.info("linear_train : %s " % (modelfile))
+    log._logger.info("linear_train : %s , %s" % (trainfile,testfile))
     log._logger.info("Loading...")
 
-    trainX = pickle.load(open(trainmatfile))
+    trainX = pickle.load(open(trainfile))
     trainy = [r[1] for r in tsv.reader(conv.redirect("data|train.dat"))]
 
     # Optimation
@@ -40,17 +40,21 @@ def linear_train(modelfile,trainmatfile,vs='1vsR',C=1,regularize='l2',gridsearch
         
     clf.fit(trainX,trainy)
     
-    log._logger.info("Dumping to %s" % (modelfile))
+    log._logger.info("Dumping to %s" % ('svm.model'))
     
-    pickle.dump(clf,open(modelfile,'w'))
+    if dumpmodel:
+        pickle.dump(clf,open("svm.model",'w'))
     
+    if testfile is not None:
+        test(clf,testfile)
+
     return clf
 
 
-def test(modelfile,testmatfile,outfile):
+def test(model,testmatfile):
     ""
     
-    clf = pickle.load(open(modelfile))
+    clf = model
     
     testX = pickle.load(open(testmatfile))
     testy = [r[1] for r in tsv.reader(conv.redirect("data|test.dat"))]
@@ -58,23 +62,23 @@ def test(modelfile,testmatfile,outfile):
     log._logger.info("Testing...")
     predicts = clf.predict(testX)
     
-    with codecs.open(outfile,'w',encoding='utf-8') as fl:
+    with codecs.open("svm.predicted.dat",'w',encoding='utf-8') as fl:
         for src,p in zip(tsv.reader(conv.redirect("data|test.dat")),predicts):
             fl.write( "%s\t%s\t%s\n" % (src[0],p, src[1]) )
+    
     
 if __name__ == "__main__":
 
     cmd = argparse.ArgumentParser()
     cmd.add_argument("--input", help="which feature you use",default="bow")
-    cmd.add_argument("--output", help="path of the output",default="svm.predicted.dat")
     cmd.add_argument("--regularize", help="regularition",default="l2")
-    cmd.add_argument("--C", help="alpha of discounting", type=float, default=1)
+    cmd.add_argument("--C", help="parameter C", type=float, default=1)
     cmd.add_argument("--vs", help="enable vs", default='1vsR')
-    cmd.add_argument("--gridsearch", help="enable gridsearch", type=bool, default=False)
     
     args = cmd.parse_args()
 
-        
-    linear_train("svm.model",trainmatfile=conv.redirect(args.input+"|train.vectorized.mat"),vs=args.vs,regularize=args.regularize,gridsearch=args.gridsearch,C=args.C)
-    test("svm.model",conv.redirect(args.input+"|test.vectorized.mat"),args.output)
+    train_feature_file = conv.redirect(args.input+"|train.vectorized.mat")
+    test_feature_file = conv.redirect(args.input+"|test.vectorized.mat")
+    
+    linear_train(trainfile=train_feature_file,testfile=test_feature_file,vs=args.vs,regularize=args.regularize,C=args.C)
     
